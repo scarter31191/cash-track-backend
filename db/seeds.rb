5a1d9acefc03e13689c7fd973f72d92aca941c74
scarter31191 <scarter31191@gmaile.com>
2021-05-31 22:03:42 -0400

    created seed data and created account model method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    Account.create(name: 'Disney', goal_amount: 2500, balance: 1500)
+    Account.create(name: 'MMA', goal_amount: 500, balance: 250)
+    Account.create(name: 'Checking', goal_amount: 1000, balance: 1500)
+
+    Transaction.create(account_id: 1, amount: 100, kind: 'deposit', date: Date.today, description: 'bi-weekly deposit')
+    Transaction.create(account_id: 2, amount: 20, kind: 'deposit', date: Date.today, description: 'bi-weekly deposit')
+    Transaction.create(account_id: 3, amount: 300, kind: 'withdraw', date: Date.today, description: 'random road trip')
