c5720dda289c2b1fec9a770075130dad706f4ba6
scarter31191 <scarter31191@gmaile.com>
2021-06-01 22:47:47 -0400

    Setup both controllers and refactored routes

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::TransactionsController < ApplicationController
+
+    beforr_action :set_account
+
+
+    def index
+        @account.transactions
+        render json: @account.transactions
+    end
+
+    def create
+        @transaction = @account.transactions.build(transaction_params)
+        if @account.update_balance(@transaction) != 'Balance too low'
+            @transaction.save
+            render json: @transaction
+        else
+            render json: {error: 'Balance too low'}
+        end
+    end
+
+    def show
+        @transaction = Transaction.find(params[:id])
+        render json: @transaction
+    end
+
+    # def destroy
+    #     @transaction = Transaction.find(params[:id])
+    #     @transaction.destroy
+    # end
+
+    private
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end
+
+
+    def transaction_params
+        params.require(:transaction).permit(:account_id, :amount, :kind, :date, :description)
+    end
+
 end
